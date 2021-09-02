8d56ea71a8130ab41dd9e25f11b1d9318583bc23
hariom-sinha58 <sinhahariom2@gmail.com>
2021-09-02 14:26:50 +0530

    added basic project architecture

---
+++ src/Components/LandingPage.js
@@ -0,0 +1,9 @@
+const LandingPage = () => {
+    return ( 
+        <div className="app-landing-page">
+            This is the landing Page..
+        </div>
+     );
+}
+ 
+export default LandingPage;
