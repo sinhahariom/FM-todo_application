7f191d08cfa5cc1fe6c6c3d9d8d2f8885d2b000a
hariom-sinha58 <sinhahariom2@gmail.com>
2021-09-06 18:45:10 +0530

    added responsive width for the Cards

--- src/Components/ToDoCard.js
+++ src/Components/ToDoCard.js
@@ -5,6 +5,7 @@ const ToDoCard = () =>{
         <div className="todo-card-wrapper">
             <div className="todo-add-new-item">fsf</div>
             <div className="todo-added-list-items">sfsf</div>
+            <div className="todo-tabs">sfsf</div>
         </div>
     )
 }
